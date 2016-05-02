487129e1731dee6095e9931ec4689c83ff8d52aa
Mark Cantrell <mark.e.cantrell@gmail.com>
2016-05-02 15:48:24 -0400

    ...

--- src/app/demo/AuthService.js
+++ src/app/demo/AuthService.js
@@ -1,98 +1,88 @@
 'use strict';
 
-var bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt-nodejs'),
+    AuthService = ( function ()  {
 
+        var passwordDB = {},
+            sessionDB  = {},
+            getUuid = function ()  {
+                var d = new Date().getTime();
 
-/**
- *
- * @type {{isauthorized, authenticate, register, invalidate}}
- */
-const AuthService = ( function ()  {
+                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                    var r = (d + Math.random() * 16) % 16 | 0;
+                    d = Math.floor(d / 16);
+                    return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+                });
+            };
 
-    var passwordDB = {},
-        sessionDB  = {};
-    /**
-     *
-     * @returns {string}
-     */
-    var getUuid = function ()  {
-        var d = new Date().getTime();
+        return {
+            /**
+             *
+             * @param username
+             * @param uuid
+             * @param callback
+             */
+            isauthorized: function (username, uuid, callback) {
 
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            var r = (d + Math.random() * 16) % 16 | 0;
-            d = Math.floor(d / 16);
-            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-        });
-    };
+                if ( username in sessionDB && (uuid = sessionDB[username])) {
+                    callback(true);
+                } else {
+                    callback(false);
+                }
 
-    return {
-        /**
-         *
-         * @param username
-         * @param uuid
-         * @param callback
-         */
-        isauthorized: function (username, uuid, callback) {
+            },
+            /**
+             *
+             * @param username
+             * @param password
+             * @param callback
+             */
+            authenticate: (username, password, callback) => {
 
-            if ( username in sessionDB && (uuid = sessionDB[username])) {
-                callback(true);
-            } else {
-                callback(false);
-            }
+                if ( username in passwordDB)  {
+                    if ( bcrypt.compareSync(password, passwordDB[username])  ) {
 
-        },
-        /**
-         *
-         * @param username
-         * @param password
-         * @param callback
-         */
-        authenticate: (username, password, callback) => {
-
-            if ( username in passwordDB)  {
-                if ( bcrypt.compareSync(password, passwordDB[username])  ) {
-
-                    var uuid = getUuid();
-                    sessionDB[username] = uuid;
-                    bus.emitMessage('authenticate', {'username': username, 'authenticated': true, 'uuid': uuid});
-                    callback(uuid);
+                        var uuid = getUuid();
+                        sessionDB[username] = uuid;
+                        GLOBAL.bus.emitMessage('authenticate', {username: username, authenticated: true, uuid: uuid});
+                        callback(uuid);
+                    } else {
+                        callback(null);
+                    }
                 } else {
                     callback(null);
                 }
-            } else {
-                callback(null);
-            }
 
-        },
-        /**
-         *
-         * @param username
-         * @param password
-         * @param callback
-         */
-        register: (username, password, callback) => {
-            if ( !(username in passwordDB)) {
-                passwordDB[username] = bcrypt.hashSync(password);
-                bus.emitMessage('register', {'username': username });
-            }
-            callback();
-        },
-        /**
-         *
-         * @param username
-         * @param uuid
-         * @param callback
-         */
-        invalidate: (username,uuid,callback)  => {
-            if ( username in sessionDB) {
-                delete passwordDB[username];
-                bus.emitMessage('invalidate', {'username': username});
-            }
+            },
+            /**
+             *
+             * @param username
+             * @param password
+             * @param callback
+             */
+            register: (username, password, callback) => {
+                if ( !(username in passwordDB)) {
+                    passwordDB[username] = bcrypt.hashSync(password);
+                    GLOBAL.bus.emitMessage('register', {username: username});
+                }
+                callback();
+            },
+            /**
+             *
+             * @param username
+             * @param uuid
+             * @param callback
+             */
+            invalidate: (username, uuid, callback)  => {
+                if ( username in sessionDB) {
+                    delete passwordDB[username];
+                    GLOBAL.bus.emitMessage('invalidate', {username: username});
+                }
 
-            callback();
-        }
-    };
+                callback();
+            }
+        };
 
-})();
+    })();
 
 module.exports = AuthService;
--- src/app/demo/EventHandler.js
+++ src/app/demo/EventHandler.js
@@ -7,32 +7,31 @@ var EventHandler = (() =>  {
     return {
         session: (websocket) => {
             websocket.on('session-start', function (data) {
-                console.log('session starting do something');
-            })
+                console.log('session starting do something', data);
+            });
 
             websocket.on('session-end', function (data) {
-                console.log('session ending do something');
-            })
+                console.log('session ending do something', data);
+            });
         },
 
-        data:  (websocket) => {
+        data: (websocket) => {
             websocket.on('data', function (data) {
                 console.log('data received do something');
                 websocket.emit('message', data);
-            })
+            });
 
-
-            bus.on('authenticate', function(data){
+            GLOBAL.bus.on('authenticate', function (data) {
                 websocket.emit('message', data);
             });
-            bus.on('register', function(data){
+            GLOBAL.bus.on('register', function (data) {
                 websocket.emit('message', data);
             });
-            bus.on('invalidate', function(data){
+            GLOBAL.bus.on('invalidate', function (data) {
                 websocket.emit('message', data);
             });
         }
-    }
+    };
 
 })();
 
